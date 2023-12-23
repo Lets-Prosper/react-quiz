37fe214ed1e21c6009ccc5c4b488234e20330874
Lets-Prosper <prosperacho83@gmail.com>
2023-12-23 20:51:11 +0100

    redundant files

--- functions/hello.mjs
+++
@@ -1,9 +0,0 @@
-// import { useQuiz } from "../src/context/QuizContext";
-
-// export default function getQuestions() {
-//   const { dispatch } = useQuiz();
-//   fetch("http://localhost:8000/questions")
-//     .then((res) => res.json())
-//     .then((data) => dispatch({ type: "dataReceived", payload: data }))
-//     .catch((err) => dispatch({ type: "dataFailed" }));
-// }
